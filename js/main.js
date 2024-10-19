9039d551a5a3ceecc203dfb17fc455098e30f33a
Felix Barcon <synguyenvan075@gmail.com>
2024-10-19 23:53:17 +0700

    FomaiWEE

--- js/main.js
+++ js/main.js
@@ -21,8 +21,7 @@ class Particle1 {
     this.canvas = canvas;
     this.life = 1000 + Math.random() * 3000;
 
-    this.x =
-      $(window).width() / 2 + (Math.random() * 20 - Math.random() * 20);
+    this.x = $(window).width() / 2 + (Math.random() * 20 - Math.random() * 20);
     this.y = $(window).height();
     this.s = 2 + Math.random();
     this.w = $(window).width();
@@ -30,9 +29,9 @@ class Particle1 {
     this.direction = this.random > 0.5 ? -1 : 1;
     this.radius = 1 + 3 * this.random;
     this.color = "#ff417d";
-
+    // this.ID = setTittle manter
     this.ID = setInterval(
-      function() {
+      function () {
         microparticles.push(
           new microParticle(c1.context, {
             x: this.x,
@@ -44,7 +43,7 @@ class Particle1 {
     );
 
     setTimeout(
-      function() {
+      function () {
         clearInterval(this.ID);
       }.bind(this),
       this.life
@@ -66,9 +65,7 @@ class Particle1 {
 
   move() {
     this.x -=
-      this.direction *
-      Math.sin(this.progress / (this.random1 * 430)) *
-      this.s;
+      this.direction * Math.sin(this.progress / (this.random1 * 430)) * this.s;
     this.y -= Math.cos(this.progress / this.h) * this.s;
 
     if (this.x < 0 || this.x > this.w - this.radius) {
@@ -134,7 +131,7 @@ class microParticle {
 var random_life = 1000;
 
 setInterval(
-  function() {
+  function () {
     particles.push(new Particle1(canvas));
     random_life = 2000 * Math.random();
   }.bind(this),
@@ -166,10 +163,10 @@ function blur(ctx, canvas, amt) {
 
 function update() {
   clear();
-  particles = particles.filter(function(p) {
+  particles = particles.filter(function (p) {
     return p.move();
   });
-  microparticles = microparticles.filter(function(mp) {
+  microparticles = microparticles.filter(function (mp) {
     return mp.move();
   });
   requestAnimationFrame(update.bind(this));
